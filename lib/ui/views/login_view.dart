440a13a43a73fb40e6e56eba161d66f148b1bce0
Santiago Quiroga <santiago.j.quiroga@gmail.com>
2020-03-31 15:32:15 -0300

    Add base view, add login view

---
+++ lib/ui/views/login_view.dart
@@ -0,0 +1,75 @@
+import 'package:flt_base/core/enums/viewstate.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flt_base/core/viewmodels/login_viewmodel.dart';
+import 'package:flt_base/ui/views/z_base_view.dart';
+
+class LoginView extends StatefulWidget {
+  @override
+  LoginViewState createState() => LoginViewState();
+}
+
+class LoginViewState extends State<LoginView> {
+  final TextEditingController username = TextEditingController(text: '');
+  final TextEditingController password = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<LoginViewModel>(
+      builder: (context, viewModel, child) => Scaffold(
+          backgroundColor: Colors.indigo,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 80.0),
+                  child: FlutterLogo(
+                    size: 150,
+                  )),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                height: 50.0,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: TextField(
+                    decoration: InputDecoration.collapsed(hintText: 'Username'),
+                    controller: username),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                height: 50.0,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: TextField(
+                    decoration: InputDecoration.collapsed(hintText: 'Password'),
+                    obscureText: true,
+                    controller: password),
+              ),
+              viewModel.state == ViewState.Busy
+                  ? CircularProgressIndicator()
+                  : FlatButton(
+                      color: Colors.white,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () async {
+                        var success =
+                            await viewModel.login(username.text, password.text);
+
+                        if (success) {
+                          Navigator.pushNamed(context, '/');
+                        }
+                      },
+                    )
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/ui/views/z_base_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:flt_base/locator.dart';
+import 'package:flt_base/core/viewmodels/z_base_viewmodel.dart';
+
+class BaseView<T extends BaseViewModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T viewModel, Widget child)
+      builder;
+  final Function(T) onViewModelReady;
+
+  BaseView({this.builder, this.onViewModelReady});
+
+  @override
+  BaseViewState<T> createState() => BaseViewState<T>();
+}
+
+class BaseViewState<T extends BaseViewModel> extends State<BaseView<T>> {
+  T viewModel = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onViewModelReady != null) {
+      widget.onViewModelReady(viewModel);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+        create: (_) => viewModel,
+        child: Consumer<T>(builder: widget.builder));
+  }
+}
