e6206186833437f8acbe72df1d23da407340070e
Santiago Quiroga <santiago.j.quiroga@gmail.com>
2020-04-20 09:46:52 -0300

    Improve viewModel initialization

--- lib/core/viewmodels/z_base_viewmodel.dart
+++ lib/core/viewmodels/z_base_viewmodel.dart
@@ -3,9 +3,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flt_base/core/enums/viewstate.dart';
 
 class BaseViewModel extends ChangeNotifier {
+  bool initialized = false;
+
   ViewState _state = ViewState.Idle;
 
   ViewState get state => _state;
+
+  void initialize() {
+    initialized = true;
+  }
 
   void setState(ViewState viewState) {
     _state = viewState;
